f721025d2212542452dbbf8f61bcf92147767ce5
Sabal <sabolkhatri@gmail.com>
2025-10-06 23:14:56 +0545

    2020 all the pages

---
+++ wp-content/themes/ultimate-himalayan/lib/js/jquery.customSelect.js
@@ -0,0 +1,177 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/*!
+ * jquery.customSelect() - v0.4.0
+ * http://adam.co/lab/jquery/customselect/
+ * 2013-04-28
+ *
+ * Copyright 2013 Adam Coulombe
+ * @license http://www.opensource.org/licenses/mit-license.html MIT License
+ * @license http://www.gnu.org/licenses/gpl.html GPL2 License 
+ */
+
+(function ($) {
+    'use strict';
+
+    $.fn.extend({
+        customSelect: function (options) {
+            // filter out <= IE6
+            if (typeof document.body.style.maxHeight === 'undefined') {
+                return this;
+            }
+            var defaults = {
+                    customClass: 'customSelect',
+                    mapClass:    true,
+                    mapStyle:    true
+            },
+            options = $.extend(defaults, options),
+            prefix = options.customClass,
+            changed = function ($select,customSelectSpan) {
+                var currentSelected = $select.find(':selected'),
+                customSelectSpanInner = customSelectSpan.children(':first'),
+                html = currentSelected.html() || '&nbsp;';
+
+                customSelectSpanInner.html(html);
+                
+                if (currentSelected.attr('disabled')) {
+                	customSelectSpan.addClass(getClass('DisabledOption'));
+                } else {
+                	customSelectSpan.removeClass(getClass('DisabledOption'));
+                }
+                
+                setTimeout(function () {
+                    customSelectSpan.removeClass(getClass('Open'));
+                    $(document).off('mouseup.'+getClass('Open'));                  
+                }, 60);
+            },
+            getClass = function(suffix){
+                return prefix + suffix;
+            };
+
+            return this.each(function () {
+                var $select = $(this),
+                    customSelectInnerSpan = $('<span />').addClass(getClass('Inner')),
+                    customSelectSpan = $('<span />');
+
+                $select.after(customSelectSpan.append(customSelectInnerSpan));
+                
+                customSelectSpan.addClass(prefix);
+
+                if (options.mapClass) {
+                    customSelectSpan.addClass($select.attr('class'));
+                }
+                if (options.mapStyle) {
+                    customSelectSpan.attr('style', $select.attr('style'));
+                }
+
+                $select
+                    .addClass('hasCustomSelect')
+                    .on('update', function () {
+						changed($select,customSelectSpan);
+						
+                        var selectBoxWidth = parseInt($select.outerWidth(), 10) -
+                                (parseInt(customSelectSpan.outerWidth(), 10) -
+                                    parseInt(customSelectSpan.width(), 10));
+						
+						// Set to inline-block before calculating outerHeight
+						customSelectSpan.css({
+                            display: 'inline-block'
+                        });
+						
+                        var selectBoxHeight = customSelectSpan.outerHeight();
+
+                        if ($select.attr('disabled')) {
+                            customSelectSpan.addClass(getClass('Disabled'));
+                        } else {
+                            customSelectSpan.removeClass(getClass('Disabled'));
+                        }
+
+                        customSelectInnerSpan.css({
+                            width:   selectBoxWidth,
+                            display: 'inline-block'
+                        });
+
+                        $select.css({
+                            '-webkit-appearance': 'menulist-button',
+                            width:                customSelectSpan.outerWidth(),
+                            position:             'absolute',
+                            opacity:              0,
+                            height:               selectBoxHeight,
+                            fontSize:             customSelectSpan.css('font-size')
+                        });
+                    })
+                    .on('change', function () {
+                        customSelectSpan.addClass(getClass('Changed'));
+                        changed($select,customSelectSpan);
+                    })
+                    .on('keyup', function () {
+                        if(!customSelectSpan.hasClass(getClass('Open'))){
+                            $select.blur();
+                            $select.focus();
+                        }
+                    })
+                    .on('mousedown', function (e) {
+                        customSelectSpan.removeClass(getClass('Changed'));
+                    })
+                    .on('mouseup', function (e) {
+                        if(!customSelectSpan.hasClass(getClass('Open'))){
+                            customSelectSpan.addClass(getClass('Open'));
+                            e.stopPropagation();
+                            $(document).one('mouseup.'+getClass('Open'), function (e) {
+                                if( e.target != $select.get(0) && $.inArray(e.target,$select.find('*').get()) < 0 ){
+                                    $select.blur();
+                                }else{
+                                    changed($select,customSelectSpan);
+                                }
+                            });
+                        }
+                    })
+                    .focus(function () {
+                        customSelectSpan.removeClass(getClass('Changed')).addClass(getClass('Focus'));
+                    })
+                    .blur(function () {
+                        customSelectSpan.removeClass(getClass('Focus')+' '+getClass('Open'));
+                    })
+                    .hover(function () {
+                        customSelectSpan.addClass(getClass('Hover'));
+                    }, function () {
+                        customSelectSpan.removeClass(getClass('Hover'));
+                    })
+                    .trigger('update');
+            });
+        }
+    });
+})(jQuery);
+
+
+}
+/*
+     FILE ARCHIVED ON 07:26:45 Jan 09, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:48:33 Oct 06, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.588
+  exclusion.robots: 0.037
+  exclusion.robots.policy: 0.027
+  esindex: 0.01
+  cdx.remote: 64.104
+  LoadShardBlock: 608.803 (3)
+  PetaboxLoader3.datanode: 548.236 (5)
+  PetaboxLoader3.resolve: 827.714 (3)
+  load_resource: 896.325 (2)
+*/
---
+++ wp-content/themes/ultimate-himalayan/lib/js/scripts.js
@@ -0,0 +1,138 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+//==============================================================
+// CUSTOM SCRIPTS
+// Author: Last Door Solutions (http://lastdoorsolutions.com)
+// 2011
+// ==============================================================
+
+
+$(document).ready(function() {
+// be smart: prevent default behaviour on links with #
+	$('a[href^="#"]').click(function(e){
+		e.preventDefault();
+	});
+// Superfish
+$('ul.main-nav').superfish({
+	delay		: 0
+}); 
+//$('span.sf-sub-indicator').remove(); 
+
+var href = window.location.href;
+if(href.indexOf('/en/')!== -1){
+	 $('.english-site').addClass('active');
+	$('.error-info a').each(function() {
+        	console.log($(this).attr('href'));
+			$(this).attr('href', $(this).attr('href')+ 'en');
+    });
+	}else{
+		$('.french-site').addClass('active');
+		$('.article-overview a.gray').css('margin-left','0');
+		$('.article-overview a.package-book').css('margin-right','20px');
+		}
+
+// Maintain Equal Height
+//$('.two-col').equalHeights(); // Add as per your requirement
+
+// get Widget title as a widget class
+
+$('.widget_text').each( function(){
+	var widgetTitle = $(this).find('.widget-title').text();
+	var widgetTitleSlug = widgetTitle.replace(/ /gi, "-");
+	widgetTitleSlug = widgetTitleSlug.toLowerCase();
+	widgetTitleSlug = "widget-" + widgetTitleSlug;
+	$(this).addClass(widgetTitleSlug);
+});
+
+
+// Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.main-slider').flexslider({
+    animation: "slide"
+  });
+});
+
+if($(".quick-message-box").length>0) {
+$(".close-btn").click( function(){
+//$(".quick-message-box").fadeOut();
+$('.quick-message-box').animate({'right': '-250'}, 1000, function(){
+			$('.quick-message-box-ico').show();
+		});
+});
+}
+
+$('.quick-message-box-ico a').click(function(){
+	$('.quick-message-box').animate({'right': '0'}, 1000, function(){
+			$('.quick-message-box-ico').hide();
+		});
+	});
+
+
+$('.highlight-hover-text').hide();
+$("ul.highlight-images li").mouseenter(function(){
+	$(this).children('.highlight-hover-text').show();
+	$(this).children().children('.inner-img').hide();
+}).mouseleave(function(){
+	$(this).children('.highlight-hover-text').hide();
+	$(this).children().children('.inner-img').show();
+});
+
+$('.fancybox').fancybox({
+	'height' : 'auto',
+	'autoDimensions' : false
+	});
+
+$(".tooltip").tipTip({
+	defaultPosition: "top"
+	});
+
+
+});
+// end ready function here.
+jQuery(function(){
+		jQuery('[id$="-all"] > ul.categorychecklist').each(function() {
+			var $list = jQuery(this);
+			var $firstChecked = $list.find(':checked').first();
+
+			if ( !$firstChecked.length )
+				return;
+
+			var pos_first = $list.find(':checkbox').position().top;
+			var pos_checked = $firstChecked.position().top;
+
+			$list.closest('.tabs-panel').scrollTop(pos_checked - pos_first + 5);
+		});
+	});
+
+
+}
+/*
+     FILE ARCHIVED ON 13:21:05 Jan 12, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:48:39 Oct 06, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.603
+  exclusion.robots: 0.043
+  exclusion.robots.policy: 0.03
+  esindex: 0.01
+  cdx.remote: 209.407
+  LoadShardBlock: 828.091 (3)
+  PetaboxLoader3.datanode: 813.814 (5)
+  PetaboxLoader3.resolve: 782.392 (3)
+  load_resource: 1300.339 (2)
+*/
